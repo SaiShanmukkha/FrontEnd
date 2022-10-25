e106c81883e6abf6fc246a5aa9b40b42a8fec831
Surapaneni Sai Shanmukkha <90093621+SaiShanmukkha@users.noreply.github.com>
2022-10-25 03:29:24 -0700

    updated

--- pages/api/staticdata.js
+++
@@ -1,8 +0,0 @@
-import path from "path";
-import { promises as fs } from "fs";
-
-export default async function handler(req, res) {
-  const jsonDirectory = path.join(process.cwd(), "data");
-  const fileContents = await fs.readFile(jsonDirectory + "/load.txt", "utf8");
-  res.status(200).send(fileContents);
-}
